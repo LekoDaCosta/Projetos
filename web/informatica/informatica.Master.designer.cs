d679d672f7e07a955aaea14a54a1964c16e56848
leandro <leandro.costa72@etec.sp.gov.br>
2017-03-03 13:40:28 -0300

    foi adicionado adRotator pagina

--- web/informatica/informatica.Master.designer.cs
+++ web/informatica/informatica.Master.designer.cs
@@ -31,6 +31,24 @@ namespace informatica {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// AdRotator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.AdRotator AdRotator1;
+        
+        /// <summary>
+        /// XmlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.XmlDataSource XmlDataSource1;
+        
+        /// <summary>
         /// ContentPlaceHolder3 control.
         /// </summary>
         /// <remarks>
